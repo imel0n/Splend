62af8c13e397be8a32498b6bc63a3a97a0c2dbc3
imel0n <hejieming09@gmail.com>
2025-05-27 13:02:13 +0800

    Added firebaseConfig.js to link the project to its Firebase database

---
+++ Splend/firebaseConfig.js
@@ -0,0 +1,19 @@
+// This code is directly copied from the "Add Firebase to your web app" page on firebase.google.com.
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDM8P-UfqlH1zUdUKGxCN3E9SuvzF1f04I",
+  authDomain: "splend-a3687.firebaseapp.com",
+  projectId: "splend-a3687",
+  storageBucket: "splend-a3687.firebasestorage.app",
+  messagingSenderId: "514224126426",
+  appId: "1:514224126426:web:c3b2cd49c198c2836c68e3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
